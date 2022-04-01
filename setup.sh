61b4627e0e99147b46052defadab945775a7cdac
NEURODATA <67514861+NeuroData-ltd@users.noreply.github.com>
2022-04-01 22:46:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+mysql --user cp1425760p08_contrapid --password 02MNeJXVsao+ --host localhost --port 3306 cp1425760p08_contrapid < "cp1425760p08_contrapid.sql"
